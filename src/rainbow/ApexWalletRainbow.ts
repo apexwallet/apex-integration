6f9a4fac398b3160ac4710c4546d766a223603d2
apexwallet <thesuperb1@Sumis-MacBook-Pro.local>
2023-02-13 11:56:25 -0500

    updating iconurl rainbow

---
+++ src/rainbow/ApexWalletRainbow.ts
@@ -0,0 +1,20 @@
+import { ApexConnector } from '../wagmi/ApexConnector';
+
+export const apexWallet = (chains) => {
+  return {
+    id: 'apex',
+    name: 'Apex',
+    iconUrl: 'https://uploads-ssl.webflow.com/62e2fb6cf3873f69b314adc7/63ea86ef42b65cdb753c3436_apex-logo.svg',
+    iconBackground: '#0F1D28',
+    downloadUrls: {
+      browserExtension: 'https://chrome.google.com/webstore/detail/apex-wallet/oppceojapmdmhpnmjpballbbdclocdhj',
+    },
+    createConnector: () => {
+      return {
+        connector: new ApexConnector({
+          chains,
+        }),
+      };
+    },
+  };
+};
